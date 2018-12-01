aa6b3bbee14075c94a87c21ec44d9a7714d32de2
Keishi Kawada <megumish@exploitouri.st>
2018-12-02 03:12:22 +0900

    event per thread, but all fn unwrap

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn base_system_in_all_is_unwrap() {
+        let mut input = Cursor::new(vec!['H' as u8, 'i' as u8]);
+        let output = Arc::new(Mutex::new(Cursor::new(Vec::new())));
+
+        {
+            let output = output.clone();
+
+            let event = move || {
+                let mut data = vec![0; 2];
+                assert_eq!(input.read(&mut data).unwrap(), 2);
+                let mut output = output.lock().unwrap();
+                let data = data;
+                assert_eq!(output.write(&data).unwrap(), 2);
+            };
+
+            let handle = thread::spawn(event);
+            handle.join().unwrap();
+        }
+
+        {
+            let output = output.clone();
+            let output = output.lock().unwrap();
+            assert_eq!(std::str::from_uft8(output.get_ref()).unwrap(), "Hi");
+        }
     }
 }
