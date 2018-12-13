038ca87c0af72f3ae0f139bad5b64f2c3b75bef0
川田 恵氏 (Kawada Keishi a.k.a megumish) <megumish@exploitouri.st>
2018-12-14 00:04:02 +0900

    enter and ping exit (#42)

--- src/lib.rs
+++ src/lib.rs
@@ -45,4 +45,46 @@ mod tests {
         assert_eq!(std::str::from_utf8(&*output)?, "Hi");
         Ok(())
     }
+
+    struct PseudoEnterExit<'a, R, W>
+    where
+        R: Read,
+        W: Write,
+    {
+        enter: &'a mut R,
+        exit: &'a mut W,
+    }
+
+    impl<'a, W, R> PseudoEnterExit<'a, R, W>
+    where
+        R: Read,
+        W: Write,
+    {
+        fn new(enter: &'a mut R, exit: &'a mut W) -> Self {
+            PseudoEnterExit { enter, exit }
+        }
+
+        fn single_run(&mut self) -> Result<(), failure::Error> {
+            let mut buf = [0; 1];
+            while let Ok(1) = self.enter.read(&mut buf) {
+                self.exit.write(&buf)?;
+            }
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn connection() -> Result<(), failure::Error> {
+        let hello = "Hello World";
+        let enter_length = hello.len();
+        let mut output = vec![0; enter_length];
+        {
+            let mut enter = Cursor::new(hello.as_bytes());
+            let mut exit = Cursor::new(&mut output);
+            let mut conn = PseudoEnterExit::new(&mut enter, &mut exit);
+            conn.single_run()?;
+        }
+        assert_eq!(std::str::from_utf8(&output)?, hello);
+        Ok(())
+    }
 }
